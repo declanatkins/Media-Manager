de01faaec78cb919b62e8b412afa0ce2f2124be2
Declan Atkins <declanatkins@gmail.com>
2020-11-27 10:33:01 +0000

    Finished validation decorator

--- backend/source/media_api/user/auth.py
+++ backend/source/media_api/user/auth.py
@@ -1,5 +1,8 @@
 from functools import wraps
+from time import time
+from flask import jsonify
 from flask import request
+from http import HTTPStatus
 from pymongo import MongoClient
 from .. import config
 from .user import User
@@ -7,7 +10,6 @@ from .user import User
 
 CLIENT = MongoClient(host=config.MONGODB_HOST, port=config.MONGODB_PORT)
 DB = CLIENT[config.MONGODB_DB_NAME]
-
 
 
 def validate_session(func):
@@ -19,4 +21,28 @@ def validate_session(func):
         try:
             _, session_id = request.headers['Authorization'].split()
         except KeyError:
-            raise ValueError('No authentication provided')
+            response = jsonify({
+                'status': HTTPStatus.FORBIDDEN,
+                'description': 'No authorization was provided'
+            })
+            return response, HTTPStatus.FORBIDDEN
+        session_collection = DB[config.MONGODB_SESSION_COLLECTION_NAME]
+        session_doc = session_collection.find_one({'_id': session_id})
+        
+        if not session_doc:
+            response = jsonify({
+                'status': HTTPStatus.FORBIDDEN,
+                'description': 'User is not in an active session'
+            })
+            return response, HTTPStatus.FORBIDDEN
+            
+        if time() > session_doc['last_access'] + config.SESSION_INACTIVE_TIMEOUT:
+            response = jsonify({
+                'status': HTTPStatus.FORBIDDEN,
+                'description': 'User\'s session has timed out'
+            })
+            session_collection.delete_one({'_id': session_id})
+            return response, HTTPStatus.FORBIDDEN
+        request.session_context = User.retrieve_validated_user(session_doc['user_name'])
+        return func(*args, **kwargs)
+    return wrapper
